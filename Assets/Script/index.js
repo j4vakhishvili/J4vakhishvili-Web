7e3a01b1af13bb587b3293143de8b2daf23d3f36
J4vakhishvili <71881956+j4vakhishvili@users.noreply.github.com>
2023-03-13 16:19:18 +0400

    responsive

--- Assets/Script/index.js
+++ Assets/Script/index.js
@@ -1,13 +1,23 @@
-//swiper.js
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    // direction: 'vertical',
-    loop: true,
-    slidesPerView: 3,
-    // spaceBetween: 30,
-    freeMode: true,
-    pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    // 320: {
+    //   slidesPerView: 2,
+    //   spaceBetween: 20
+    // },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 2,
+    //   spaceBetween: 30
+    },
+    // when window width is >= 640px
+    1420: {
+      slidesPerView: 3,
     }
-});
+  }
+})
